dd03f6bf2b1815eb6878decef8735f27a2385602
Nilagrib Ray <nilagrib2004@gmail.com>
2025-02-02 03:07:41 +0530

    Update Commit

---
+++ 25. Dynamic Programming/152_Maximum_Product_Subarray.py
@@ -0,0 +1,20 @@
+from typing import List
+
+def maxProduct(nums: List[int]) -> int:
+    if not nums:
+        return 0
+    max_product = nums[0]
+    min_product = nums[0]
+    result = nums[0]
+
+    for i in range(1, len(nums)):
+        if nums[i] < 0:
+            max_product, min_product = min_product, max_product
+        max_product = max(nums[i], max_product*nums[i])
+        min_product = min(nums[i], min_product*nums[i])
+        result = max(result, max_product)
+    
+    return result
+
+if __name__ == "__main__":
+    print(maxProduct([2, 3, -2, 4]))
